8a4fd4b18187e663e262b9ef8af45a8d3d7e59ec
Paul Asmuth <paul@asmuth.com>
2017-07-10 01:03:11 +0200

    add boot animation

--- firmware/controller/firmware.cc
+++ firmware/controller/firmware.cc
@@ -16,17 +16,48 @@ ISR(USART_RX_vect) {
   serial_port.handleInterrupt();
 }
 
-int main(void) {
-  for (uint32_t n = 0; ; n++) {
-    for (uint32_t i = 0; i < 16; i++) {
-      led_controller.setButton(i, i == (n % 10));
+void bootanim() {
+  for (uint8_t n = 0; n < 5; n++) {
+    led_controller.setButton(4 - n, 1);
+    led_controller.setButton((4 - n) + 5, 1);
+    for (uint8_t i = 0; i < 50; ++i) {
+      auto b = (n * 50) + i;
+      led_controller.setAmbientColour(b, b, b);
+      led_controller.refreshDisplay();
+      _delay_ms(5);
+    }
+  }
+
+  for (uint8_t n = 0; n < 5; n++) {
+    led_controller.setButton(4 - n, 1);
+    led_controller.setButton((4 - n) + 5, 1);
+    for (uint8_t i = 0; i < 50; ++i) {
+      auto b = 250 - ((n * 50) + i);
+      led_controller.setAmbientColour(b, b, b);
+      led_controller.refreshDisplay();
+      _delay_ms(5);
+    }
+  }
+
+  for (;;) {
+    for (uint8_t n = 0; n < 5; n++) {
+      led_controller.setButton(4 - n, 0);
+      led_controller.setButton((4 - n) + 5, 0);
+      led_controller.refreshDisplay();
+      _delay_ms(200);
     }
 
-    auto b = (n % 2) * 30;
-    led_controller.setAmbientColour(b, b, b);
-    led_controller.refreshDisplay();
-    _delay_ms(200);
+    for (uint8_t n = 0; n < 5; n++) {
+      led_controller.setButton(4 - n, 1);
+      led_controller.setButton((4 - n) + 5, 1);
+      led_controller.refreshDisplay();
+      _delay_ms(200);
+    }
   }
+}
+
+int main(void) {
+  bootanim();
 
   return 0;
 }
